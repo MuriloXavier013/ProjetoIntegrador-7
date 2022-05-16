0b4dcee559228be2930b960054f5683186ec036a
MuriloXavier013 <muriloxavier1001@gmail.com>
2022-05-16 03:06:00 -0300

    Slider adicionado

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', ejs)
-app.use(express.static(__dirname+ 'public'))
+app.use(express.static(__dirname + 'public'))
 
-app.get('/', (req, res)=>{
-    res.send('Retorno da index')
+app.get('/', (req, res) => {
+  res.send('Retorno da index')
 })
 
-
-
-app.listen(3030, () =>{
-    console.log('Rodando na porta 3030')
+app.listen(3030, () => {
+  console.log('Rodando na porta 3030')
 })
