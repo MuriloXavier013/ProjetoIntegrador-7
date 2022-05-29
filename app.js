53dd6ff5203a32201096cafa274960f9452f99ac
Inglide Noberto <inglidenoberto961@gmail.com>
2022-05-29 18:38:34 -0300

    version1

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express')
 const ProductsRoutes = require('./src/routers/ProductsRoutes')
 const app = express()
 
+// ****************** Configurando o EJS  ********************************//
+app.set('view engine', 'ejs')
+app.set(express.static('public'))
+
 app.use('/products' , ProductsRoutes)
+
+/*app.get('/' , (res, req) => {
+  res.render('index')
+})*/
 
 app.listen(3030, () => {
   console.log('Rodando na porta 3030')
